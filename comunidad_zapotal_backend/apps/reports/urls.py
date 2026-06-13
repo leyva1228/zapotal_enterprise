bd964ebaa1edb0f0c95543c951408db9a22b0229
JohnLeyva <john.leyva@tecsup.edu.pe>
2026-06-13 01:11:13 -0500

    refresh gitignore

--- comunidad_zapotal_backend/apps/reports/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import ContactoMensajeViewSet, LibroReclamacionViewSet
-
-router = DefaultRouter()
-router.register('contacto-mensajes', ContactoMensajeViewSet, basename='contactomensaje')
-router.register('libro-reclamaciones', LibroReclamacionViewSet, basename='libro-reclamacion')
-
-urlpatterns = router.urls
